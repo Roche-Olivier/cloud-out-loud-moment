c7aefac5209932999cb8088755d2820fb11bd72c
Roche Olivier <rolivier@mmltd.co.za>
2024-02-28 23:19:47 +0200

    Updated the methods to have a couple more
    Updated the methods to have a couple more

--- index.js
+++ index.js
@@ -8,5 +8,17 @@ exports._moment = {
     utc_now: function() {
         var current_formatted_time = moment.utc(new Date()).utcOffset(120).format("YYYY-MM-DD HH:mm:ss")
         return current_formatted_time
+    },
+    formatted: function(date_time) {
+        var current_formatted_time = moment(date_time).format("YYYY-MM-DD HH:mm:ss")
+        return current_formatted_time
+    },
+    utc_formatted: function(date_time) {
+        var current_formatted_time = moment.utc(date_time).utcOffset(120).format("YYYY-MM-DD HH:mm:ss")
+        return current_formatted_time
+    },
+    file_formatted: function(date_time) {
+        var current_formatted_time = moment(new Date()).format("YYYYMMDDHHmmssSSSS")
+        return current_formatted_time
     }
 }
